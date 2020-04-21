85793c88c3847852ecffe9fe6a44927e997161c2
Yevhenii Bakai <Yevhenii.Bakai@onseo.biz>
2020-04-21 13:43:56 +0300

    implemented print service

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,17 @@
 import { Component } from '@angular/core';
+import { PrintService } from './print.service';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  templateUrl: './app.component.html'
 })
 export class AppComponent {
-  title = 'print-service';
+  title = 'angular-print-service';
+
+  constructor(public printService: PrintService) { }
+
+  onPrintInvoice() {
+    const invoiceIds = ['101', '102'];
+    this.printService.printDocument('invoice', invoiceIds);
+  }
 }
